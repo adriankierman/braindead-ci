cab7759f894e784f39fcb4e48df8798d12ca9c8a
mpl-giant-robot <oknox@kiermansystems.com>
2014-08-25 01:53:55 +0000

    Update for prepassed payload

--- routes/handleGithubWebhook.js
+++ routes/handleGithubWebhook.js
@@ -14,15 +14,20 @@ var Job = require('../lib/job')
 module.exports = function (req, res, next) {
   db.settings.findOne({ type: 'generalSettings' }, function (err, settings) {
     if (req.query.token === undefined || req.query.token.length === 0 || req.query.token !== settings.githubToken) { return res.send(200); }
-
+    console.log(req.body);
     db.jobs.find({}, function (err, jobs) {
-      var payload = JSON.parse(req.body.payload)
+      var payload = req.body
         , receivedGithubRepoUrl = payload.repository.url
         , receivedBranch = payload.ref.replace(/^.*\//,'')
         ;
 
+      console.log('repo '+ receivedGithubRepoUrl);
+      console.log('branch '+ receivedBranch);
       // Build all the enabled jobs corresponding using the repo and branch of this push
       jobs.forEach(function (job) {
+        console.log('job repo '+ job.githubRepoUrl);
+        console.log('job branch '+ job.branch);
+
         if (job.githubRepoUrl === receivedGithubRepoUrl && job.branch === receivedBranch) {
           if (job.enabled) {
             executor.registerBuild(job.name);
