7f07be8f29695fea4bad66d48609ac2c2e608089
adriankierman <adriankierman@hotmail.com>
2014-09-09 00:02:14 -0700

    Added forever

---
+++ forever.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# forever runs server in background, manages logs, and auto-restarts on crash.
+# Useful for running in AWS.
+
+set -e
+FOREVER=./node_modules/forever/bin/forever
+$FOREVER stopall || true
+
+$FOREVER start -l forever.log -o out.log -e err.log braindead.js
