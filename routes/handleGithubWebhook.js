0ed2ff74aa7e88c5766414f13724db61af8992c8
adriankierman <adriankierman@hotmail.com>
2015-07-09 12:37:59 -0400

    Fixup github webhook

--- routes/handleGithubWebhook.js
+++ routes/handleGithubWebhook.js
@@ -14,22 +14,27 @@ var Project = require('../lib/project')
 module.exports = function (req, res, next) {
   db.settings.findOne({ type: 'generalSettings' }, function (err, settings) {
     if (req.query.token === undefined || req.query.token.length === 0 || req.query.token !== settings.githubToken) { return res.send(200); }
-
-    db.projects.find({}, function (err, projects) {
-      var payload = JSON.parse(req.body.payload)
+    console.log(req.body);
+    db.jobs.find({}, function (err, jobs) {
+      var payload = req.body
         , receivedGithubRepoUrl = payload.repository.url
         , receivedBranch = payload.ref.replace(/^.*\//,'')
         ;
 
-      // Build all the enabled projects corresponding using the repo and branch of this push
-      projects.forEach(function (project) {
-        if (project.githubRepoUrl === receivedGithubRepoUrl && project.branch === receivedBranch) {
-          if (project.enabled) {
-            executor.registerBuild(project.name);
+      console.log('repo '+ receivedGithubRepoUrl);
+      console.log('branch '+ receivedBranch);
+      // Build all the enabled jobs corresponding using the repo and branch of this push
+      jobs.forEach(function (job) {
+        console.log('job repo '+ job.githubRepoUrl);
+        console.log('job branch '+ job.branch);
+
+        if (job.githubRepoUrl === receivedGithubRepoUrl && job.branch === receivedBranch) {
+          if (job.enabled) {
+            executor.registerBuild(job.name);
           } else {
-            Project.getProject(project.name, function (err, project) {
-              if (err || !project) { return; }
-              project.advertiseBuildResult(null);
+            Job.getJob(job.name, function (err, job) {
+              if (err || !job) { return; }
+              job.advertiseBuildResult(null);
             });
           }
         }
